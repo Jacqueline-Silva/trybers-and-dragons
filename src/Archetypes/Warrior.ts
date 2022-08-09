fa62c994de2b197ca3084b638e763f5cc25cda54
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-09 18:37:02 -0300

    :sparkles: feat: Criando quatro subsclasses da classe Archetypes

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static _archeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior._archeInstances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._archeInstances;
+  }
+}
