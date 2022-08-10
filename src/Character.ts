1645eb2e58bbcefd38351bbca0ba53bf728d5609
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-09 22:38:51 -0300

    :sparkles: feat: Criando a classe Character - concluído

--- src/Character.ts
+++ src/Character.ts
@@ -31,10 +31,40 @@ export default class Character implements Fighter {
   }
 
   get race(): Race { return this._race; }
-  get archetypes(): Archetype { return this._archetype; }
+  get archetype(): Archetype { return this._archetype; }
   get lifePoints(): number { return this._lifePoints; }
   get strength(): number { return this._strength; }
   get defense(): number { return this._defense; }
   get dexterity(): number { return this._dexterity; }
-  get energy(): Energy { return this._energy; }
+  get energy(): Energy { 
+    return {
+      type_: this._energy.type_,
+      amount: this._energy.amount,
+    };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense;
+    if (damage > 0) this._lifePoints -= damage;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+
+    this._lifePoints = this._maxLifePoints;
+  }
 }
