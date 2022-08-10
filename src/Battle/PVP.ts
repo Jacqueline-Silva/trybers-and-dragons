ba481d7f9b4b3ceb5e6b3d5f416c9e352d35f232
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-10 16:12:54 -0300

    :sparkles: feat: Criando a classe de batalha PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,21 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _player1: Fighter;
+  private _player2: Fighter;
+
+  constructor(player: Fighter, player2: Fighter) {
+    super(player);
+    this._player1 = player;
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    if (this._player1.lifePoints !== -1 || this._player2.lifePoints !== -1) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    }
+    return super.fight();
+  }
+}
