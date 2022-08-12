4471eed673899ae6b516c9a495dd5a735ee08084
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-12 15:19:45 -0300

    :sparkles: fix: batalha PVE

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -5,16 +5,16 @@ export default class PVE extends Battle {
   private _player1: Fighter;
   private _monsters: SimpleFighter[];
 
-  constructor(player: Fighter, [monster1, monster2]: SimpleFighter[]) {
+  constructor(player: Fighter, monsters: SimpleFighter[]) {
     super(player);
     this._player1 = player;
-    this._monsters = [monster1, monster2];
+    this._monsters = monsters;
   }
 
   fight(): number {
     this._monsters.forEach((monster) => {
-      while (!(this._player1.lifePoints === -1
-        && monster.lifePoints === -1)) {
+      while (this._player1.lifePoints > 0
+        && monster.lifePoints > 0) {
         this._player1.attack(monster);
         monster.attack(this._player1);
       }
--- src/Monster.ts
+++ src/Monster.ts
@@ -13,7 +13,7 @@ export default class Monster implements SimpleFighter {
   get strength(): number { return this._strength; }
 
   receiveDamage(attackPoints: number): number {
-    const damage = this._lifePoints - attackPoints;
+    const damage = attackPoints - this._lifePoints;
     if (damage > 0) this._lifePoints -= damage;
     if (this._lifePoints <= 0) this._lifePoints = -1;
     return this._lifePoints;
